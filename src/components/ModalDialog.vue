a9aab21233c2cab67d2d46eb978ca9ac898892d8
Shikataramuno <nomura.takashi@maia.eonet.ne.jp>
2018-11-29 22:34:28 +0900

    モーダルダイアログの基本動作を実装した。記事にあげるまでに、もう少し見た目を飾ること

---
+++ src/components/ModalDialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">モーダルダイアログ
+          </div>
+          <div class="modal-body">
+            <input type='text' v-model='message' placeholder="メッセージ入力欄です">
+          </div>
+          <div class="modal-footer">
+            <button class="modal-default-button" @click='ok(message)'>OK</button>
+            <button class="modal-default-button" @click='cancel()'>Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Vue } from 'vue-property-decorator';
+
+export default class ModalDialog extends Vue {
+  message: string = '';
+
+  @Emit('ok')
+  ok(str: string): void {
+    console.log('ok @ dialog');
+    console.log(str);
+  }
+  @Emit('cancel')
+  cancel(): void {
+    console.log('cancel @ dialog');
+  }
+}
+</script>
+
+<style scoped>
+@import '../style/modal.css'
+</style>
---
+++ src/components/ModalDialogSample.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="ModalDialogSampleBase">
+    <h1>{{ msg }}</h1>
+    <button @click="show" :diable='!showDialog'>ダイアログの表示</button>
+    <div>{{message}}</div>
+    <ModalDialog v-if="showDialog" @ok='ok' @cancel='cancel'/>
+  </div>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import ModalDialog from './ModalDialog.vue';
+
+@Component({
+  components: {
+    ModalDialog,
+  },
+})
+export default class ModalDialogSample extends Vue {
+  name: string = 'ModalDialogSample';
+  message: string = '';
+  showDialog: boolean = false;
+  @Prop() private msg!: string;
+  // computed
+  // methods
+  show(): void {
+    console.log('showDialog');
+    this.showDialog = true;
+  }
+  ok(message: string): void {
+    console.log('close with OK');
+    console.log(message);
+    this.showDialog = false;
+    this.message = message;
+  }
+  cancel(): void {
+    console.log('close with Cancel');
+    this.showDialog = false;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
